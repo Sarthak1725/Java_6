1c0c73b98f8cace513c841ef8a522bef422c9fce
sarthak <156622637+Sarthak1725@users.noreply.github.com>
2025-03-08 23:26:39 +0530

    Comments added

--- DecoyDuck.java
+++ DecoyDuck.java
@@ -1,3 +1,6 @@
+// DecoyDuck.java 
+
+// A wooden duck that neither swims, flies, nor quacks
 public class DecoyDuck extends Duck {
     public DecoyDuck() {
         super(new NotFly(), new Drown(), new MuteQuack());
