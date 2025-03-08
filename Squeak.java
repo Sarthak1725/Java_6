6f0eab8a0bc255a3fb3e66c10fbcfcedea2a06dc
sarthak <156622637+Sarthak1725@users.noreply.github.com>
2025-03-08 23:37:36 +0530

    Implements squeaky quack behavior

---
+++ Squeak.java
@@ -0,0 +1,7 @@
+//Implements squeaky quack behavior
+public class Squeak implements QuackBehavior {
+    @Override
+    public void quack() {
+        System.out.println("Squeak! Squeak!");
+    }
+}
